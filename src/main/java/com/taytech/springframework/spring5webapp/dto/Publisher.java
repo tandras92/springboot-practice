d36600324fa03ddf08ab81582384443c801a30f7
Taylor <taymarfor@gmail.com>
2021-04-14 15:13:00 -0500

    created publisher entity

--- src/main/java/com/taytech/springframework/spring5webapp/dto/Publisher.java
+++ src/main/java/com/taytech/springframework/spring5webapp/dto/Publisher.java
@@ -1,2 +1,84 @@
-package com.taytech.springframework.spring5webapp.dto;public class Publisher {
+package com.taytech.springframework.spring5webapp.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Publisher {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    @NotNull
+    private String zipcode;
+
+    @NotNull
+    private Set<Book> books = new HashSet<>();
+
+    public Publisher(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
