9df6471ca1ed9e3fad412969d8c372991dfb853a
Taylor <taymarfor@gmail.com>
2021-04-14 10:00:19 -0500

    created book entity

--- src/main/java/com/taytech/springframework/spring5webapp/model/BookEntity.java
+++ src/main/java/com/taytech/springframework/spring5webapp/model/BookEntity.java
@@ -1,2 +1,24 @@
-package com.taytech.springframework.spring5webapp.model;public class BookEntity {
+package com.taytech.springframework.spring5webapp.model;
+
+import com.taytech.springframework.spring5webapp.dto.Author;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.util.Set;
+
+@Entity
+public class BookEntity {
+
+    @EmbeddedId
+    private BookPK bookPK;
+
+    @Column(name = "book_title", nullable = false, length = 75)
+    private String title;
+
+    private String isbn;
+
+    private String genre;
+
+    private Set<Author> authors;
 }
