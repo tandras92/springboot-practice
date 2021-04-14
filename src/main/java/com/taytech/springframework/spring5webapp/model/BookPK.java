c66317d12c53717de99d04d873c0f5a3094fdaee
Taylor <taymarfor@gmail.com>
2021-04-14 09:59:45 -0500

    created book primary key

--- src/main/java/com/taytech/springframework/spring5webapp/model/BookPK.java
+++ src/main/java/com/taytech/springframework/spring5webapp/model/BookPK.java
@@ -1,2 +1,31 @@
-package com.taytech.springframework.spring5webapp.model;public class BookPK {
+package com.taytech.springframework.spring5webapp.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class BookPK implements Serializable {
+
+    private static final long serialVersionUID = 5340546996459134482L;
+
+    @GeneratedValue
+    @Column(name = "book_id", nullable = false, length = 36)
+    private UUID bookId;
+
+    @Column(name = "record_create_ts", nullable = false)
+    private LocalDateTime recordCreateTime;
+
+    public BookPK(final UUID bookId, final LocalDateTime recordCreateTime) {
+        this.bookId = bookId;
+        this.recordCreateTime = recordCreateTime;
+    }
 }
