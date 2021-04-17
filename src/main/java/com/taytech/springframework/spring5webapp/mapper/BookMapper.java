187e563db01a3bb0647ff4274b4f8b0153764cd4
Taylor <taymarfor@gmail.com>
2021-04-17 08:16:57 -0500

    added conversion methods

--- src/main/java/com/taytech/springframework/spring5webapp/mapper/BookMapper.java
+++ src/main/java/com/taytech/springframework/spring5webapp/mapper/BookMapper.java
@@ -1,7 +1,13 @@
 package com.taytech.springframework.spring5webapp.mapper;
 
+import com.taytech.springframework.spring5webapp.dto.BookDto;
+import com.taytech.springframework.spring5webapp.model.BookEntity;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface BookMapper {
+
+    BookDto convertBookEntityToBookDto(BookEntity bookEntity);
+
+    BookEntity convertBookDtoToBookEntity(BookDto bookDto);
 }
