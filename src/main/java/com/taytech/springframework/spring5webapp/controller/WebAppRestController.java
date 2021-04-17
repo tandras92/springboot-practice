4f9ee33dc9bf737da1f59d0b2cfd8e1bc51d9628
Taylor <taymarfor@gmail.com>
2021-04-17 07:19:53 -0500

    added GET book

--- src/main/java/com/taytech/springframework/spring5webapp/controller/WebAppRestController.java
+++ src/main/java/com/taytech/springframework/spring5webapp/controller/WebAppRestController.java
@@ -1,19 +1,33 @@
 package com.taytech.springframework.spring5webapp.controller;
 
+import com.taytech.springframework.spring5webapp.dto.BookDto;
 import com.taytech.springframework.spring5webapp.service.AuthorService;
 import com.taytech.springframework.spring5webapp.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+@RequestMapping("/api/v1/library/catalog")
 @RestController
 public class WebAppRestController {
 
-    @Autowired
-    private AuthorService authorService;
+    private final AuthorService authorService;
 
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
+    public WebAppRestController(AuthorService authorService, BookService bookService) {
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
 
+    @GetMapping({"/{bookId}"})
+    public ResponseEntity<BookDto> getBook(@PathVariable UUID bookId) {
+        return new ResponseEntity<>(bookService.getBookById(bookId), HttpStatus.OK);
+    }
 
 }
