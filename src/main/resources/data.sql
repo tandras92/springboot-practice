b77ff60572ba76fa2bf6515fcd056bd8691232fd
Taylor <tandras92@outlook.com>
2021-06-09 20:02:08 -0500

    commented out insert statement

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,11 @@
 -- VALUES ('c1027673-9ebf-4fa1-bec0-7a722e43fbb7', '0132 Spencer Radial',
 --         'South Billland', 'Brill Publishers', 'FL', '30476');
 --
- INSERT INTO book (book_id, record_create_ts, book_genre, book_isbn, book_title,
-                   publisher_id)
- VALUES ('2b0beb26-a342-4db1-a32b-8e35160c02bb',
-         '2020-04-20T14:30:40.100', 'suspense', '9783161484100',
-         'Shaken, Not Stirred', '4f273973-774e-497f-9b72-6730659b21ac');
+-- INSERT INTO book (book_id, record_create_ts, book_genre, book_isbn, book_title,
+--                   publisher_id)
+-- VALUES ('2b0beb26-a342-4db1-a32b-8e35160c02bb',
+--         '2020-04-20T14:30:40.100', 'suspense', '9783161484100',
+--         'Shaken, Not Stirred', '4f273973-774e-497f-9b72-6730659b21ac');
 
 SELECT * FROM book;
 SELECT * FROM publisher;
